8ddeb613266fbd80ba3981f4f16d6dfdf3a2fa7f
David Barbosa <david.barbosa-g@mail.escuelaing.edu.co>
2025-06-28 13:23:51 -0500

    Primer commit: estructura del proyecto Spring Boot

---
+++ src/main/java/com/eci/ARSW/IntroReact/App.java
@@ -0,0 +1,13 @@
+package com.eci.ARSW.IntroReact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/eci/ARSW/IntroReact/AppTest.java
@@ -0,0 +1,12 @@
+package com.eci.ARSW.IntroReact;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
